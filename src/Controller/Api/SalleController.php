26bf5325650a80bb1202b7209c418117bce233bc
Blobaxox <leoguirauton@gmail.com>
2021-03-08 12:22:26 +0100

    Ajout salle index et show

--- src/Controller/Api/SalleController.php
+++ src/Controller/Api/SalleController.php
@@ -4,17 +4,36 @@ namespace App\Controller\Api;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Repository\SalleRepository;
+use App\Entity\Salle;
+/**
+ * @Route("/api/salle", name="api_salle_")
+ */
 class SalleController extends AbstractController
 {
-    /**
-     * @Route("/api/salle", name="api_salle")
-     */
-    public function index(): Response
+  /**
+   * @Route("", name="index")
+   */
+    public function index(SalleRepository $repository): Response
     {
-        return $this->render('api/salle/index.html.twig', [
-            'controller_name' => 'SalleController',
-        ]);
+        $salle =$repository->findAll();
+        return $this->json($salle);
+    }
+
+    /**
+     * @Route("/{id}", name="show", methods={"GET"})
+     */
+    public function show(Salle $salle = null): JsonResponse
+    {
+      if (is_null($salle)){
+        return $this->json([
+          'message' => 'Tu ne le sais pas encore, mais cette salle est déjà morte',
+          'illustration' => 'https://media.tenor.com/images/ab89c542710a01a2f8467952b76945c6/tenor.gif',
+        ], 404);
+      }
+
+      return $this->json($salle);
     }
 }
