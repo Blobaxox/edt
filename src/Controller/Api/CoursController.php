af189317b0eed2190118d27a7b790f93d90bbee2
Blobaxox <leoguirauton@gmail.com>
2021-03-08 11:46:23 +0100

    ajout show

--- src/Controller/Api/CoursController.php
+++ src/Controller/Api/CoursController.php
@@ -7,17 +7,25 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CoursRepository;
+use App\Entity\Cours;
 /**
  * @Route("/api/cours", name="api_cours_")
  */
 class CoursController extends AbstractController
 {
     /**
-     * @Route("", name="index")
+     * @Route("", name="index", methods={"GET"})
      */
     public function index(CoursRepository $repository): JsonResponse
     {
         $cours = $repository->findAll();
         return $this->json($cours);
     }
+    /**
+     * @Route("/{id}", name="show", methods={"GET"})
+     */
+    public function show(CoursRepository $repository): JsonResponse
+    {
+    }
+
 }
